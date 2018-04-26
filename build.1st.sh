2ba6619c1b8f93fa8c6d1aabcea700ae4c4c333b
Yuki Takeya <yuki@gakkougurashi.net>
2018-04-26 16:23:39 +0900

    AArch64 gcc compile fix

--- build.1st.sh
+++ build.1st.sh
@@ -64,9 +64,12 @@ do
 done
 
 case $(uname -m) in
-  x86_64|aarch64)
+  x86_64)
     sed -e '/m64=/s/lib64/lib/' \
         -i.orig gcc/config/i386/t-linux64
+  aarch64)
+	sed -e '/mabi.lp64=/s/lib64/lib/' \
+        -i.orig gcc/config/aarch64/t-aarch64-linux
  ;;
 esac
 
